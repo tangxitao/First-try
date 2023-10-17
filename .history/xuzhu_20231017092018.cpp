147a5876f934d4355004b38174b0436759ddde3a
tangxitao <2682585843@qq.com>
2023-10-17 09:33:29 +0800

    删除哦

--- .history/xuzhu_20231017092018.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello Tangxitao!\n");
-    printf("хавЃзг\n");
-    return 0;
-}
-
-
--- .history/xuzhu_20231017092843.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello Tangxitao!\n");
-    printf("åĞÒ£×ÓÀîÇïË®¶¡´ºÇï\n");
-    return 0;
-}
-
-
